6bbcdf66640fc8e3d16eddab47879b21428d9784
= <=>
2017-02-10 01:32:53 +0100

    Setting up project

---
+++ app/components/ListProducts.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class ListProducts extends React.Component{
+
+    constructor(){
+        super();
+    }
+    render(){
+        return(
+            <div id="listProducts">
+
+            </div>
+        )
+    }
+}
+
+export default ListProducts
